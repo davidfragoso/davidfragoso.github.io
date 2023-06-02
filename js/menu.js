5bfa24b632a86f157ee5daa4cf9f3f5d778a73c1
lorddeivis <fragosomadera99@gmail.com>
2023-06-02 14:22:37 -0500

    Actualización 2.0 añadido inicio de sesión con JS

--- js/menu.js
+++ js/menu.js
@@ -34,16 +34,6 @@ function closeDropdown(dropdown) {
   dropdown.classList.remove("nav__inner--show");
 }
 
-function isActive(element, string) {
-  return element.classList.value.includes(string);
-}
-
-function closeDropdown(currentDropdown) {
-  if (isActive(currentDropdown, "nav__inner--show")) {
-    currentDropdown.classList.remove("nav__inner--show");
-  }
-}
-
 window.addEventListener("resize", () => {
   closeDropdown(currentDropdown);
 
@@ -62,23 +52,134 @@ const spinner = document.getElementById("spinner");
 function mostrarModal() {
   document.getElementById("modal").style.display = "block";
 }
+
 function cerrarModal() {
   document.getElementById("modal").style.display = "none";
+}
+
+function mostrarModalLogin() {
+  document.getElementById("modal-login").style.display = "block";
+}
+
+function cerrarModalLogin() {
+  document.getElementById("modal-login").style.display = "none";
+}
+
+function mostrarNombreUsuario(username) {
+  var navUsername = document.getElementById("nav-username");
+  navUsername.textContent = "¡Bienvenido - " + username + "!";
+
+  var opcionesInicioSesion = document.getElementsByClassName("nav__login-buttons");
+  for (var i = 0; i < opcionesInicioSesion.length; i++) {
+    opcionesInicioSesion[i].style.display = "none"; // Ocultar los botones de inicio de sesión y registro
+  }
+
+  navUsername.style.display = "block"; // Mostrar el elemento "nav-username"
+
+  var btnCerrarSesion = document.getElementsByClassName("nav__logout");
+  for (var i = 0; i < btnCerrarSesion.length; i++) {
+    btnCerrarSesion[i].style.display = "block"; // Mostrar el botón de cerrar sesión en bloque
+  }
+}
+
+function iniciarSesion() {
+  var username = document.getElementById("login-username").value;
+  
+  spinner.style.display = "block";
+  contenedor.style.display = "none";
+  
+  mostrarNombreUsuario(username);
+  localStorage.setItem("username", username);
+  cerrarModalLogin();
+
+  // Restablecer el formulario
+  document.getElementById("login-form").reset();
+
+  // Reemplazar la URL actual sin agregar una nueva entrada en el historial
+  history.replaceState({}, document.title, window.location.pathname);
+  
+  // Ocultar spinner después de un breve período de tiempo
+  setTimeout(() => {
+    spinner.style.display = "none";
+    contenedor.style.display = "block";
+  }, 2000);
+}
+
+function cerrarSesion() {
+  localStorage.removeItem("username"); // Eliminar el nombre de usuario del almacenamiento local
+  
+  spinner.style.display = "block";
+  contenedor.style.display = "none";
+  
+  var opcionesInicioSesion = document.getElementsByClassName("nav__login-buttons");
+  for (var i = 0; i < opcionesInicioSesion.length; i++) {
+    opcionesInicioSesion[i].style.display = "block"; // Mostrar los botones de inicio de sesión y registro
+  }
+
+  var navUsername = document.getElementById("nav-username");
+  navUsername.textContent = ""; // Borrar el contenido del nombre de usuario
+  navUsername.style.display = "none"; // Ocultar el elemento "nav-username"
+
+  var btnCerrarSesion = document.getElementsByClassName("nav__logout");
+  for (var i = 0; i < btnCerrarSesion.length; i++) {
+    btnCerrarSesion[i].style.display = "none"; // Ocultar el botón de cerrar sesión
+  }
+
+  // Reemplazar la URL actual sin agregar una nueva entrada en el historial
+  history.replaceState({}, document.title, window.location.pathname);
+  
+  // Ocultar spinner después de un tiempo de espera
+  setTimeout(() => {
+    spinner.style.display = "none";
+    contenedor.style.display = "block";
+  }, 2000);
+}
+
+window.addEventListener("load", function () {
+  var username = localStorage.getItem("username");
+  if (username) {
+    mostrarNombreUsuario(username);
+  } else {
+    var btnCerrarSesion = document.getElementsByClassName("nav__logout");
+    for (var i = 0; i < btnCerrarSesion.length; i++) {
+      btnCerrarSesion[i].style.display = "none"; // Ocultar el botón de cerrar sesión
+    }
+  }
+});
+
+document.getElementById("login-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  iniciarSesion();
+});
+
+function mostrarModalRegistro() {
+  document.getElementById("modal-registro").style.display = "block";
+}
+
+function cerrarModalRegistro() {
+  document.getElementById("modal-registro").style.display = "none";
 }
 
 function mostrarModalContacto() {
   document.getElementById("modal-contacto").style.display = "block";
 }
+
 function cerrarModalContacto() {
   document.getElementById("modal-contacto").style.display = "none";
 }
 
+var typed;
+
 function mostrarModalAcerca() {
   document.getElementById("modal-acerca").style.display = "block";
 
-  var typed = new Typed(".typed", {
+  if (typed) {
+    typed.destroy(); //Esto es para destruir una instancia antes inicializada
+  }
+
+  typed = new Typed(".typed", {
     strings: ['<i class="nombre">David Uriel Fragoso Madera</i>'],
-    typeSpeed: 65,
+    typeSpeed: 55,
     startDelay: 300,
     smartBackspace: true,
     shuffle: false,
@@ -98,5 +199,5 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     spinner.style.display = "none";
     contenedor.style.display = "block";
-  }, 1000);
+  }, 1500);
 });
